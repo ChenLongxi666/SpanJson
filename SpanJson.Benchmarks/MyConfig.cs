90bc5d9fa1a1671c8a4f792574d41d3bf98dc1b2
Tornhoof <Tornhoof@gmail.com>
2018-11-15 11:26:22 +0100

    changes

--- SpanJson.Benchmarks/MyConfig.cs
+++ SpanJson.Benchmarks/MyConfig.cs
@@ -2,6 +2,7 @@
 using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Toolchains.CustomCoreClr;
 
 namespace SpanJson.Benchmarks
 {
@@ -9,9 +10,10 @@ namespace SpanJson.Benchmarks
     {
         public MyConfig()
         {
-            Add(Job.Default);
+            Add(Job.Default.With(CustomCoreClrToolchain.CreateBuilder().UseCoreClrNuGet("3.0.0-preview1-27101-02").UseCoreFxNuGet("4.6.0-preview1-27101-02")
+                .RuntimeFrameworkVersion("netcoreapp3.0").ToToolchain()));
             Add(MemoryDiagnoser.Default);
-            Set(new DefaultOrderProvider(SummaryOrderPolicy.Default, MethodOrderPolicy.Alphabetical));
+            Set(new DefaultOrderer(SummaryOrderPolicy.Default, MethodOrderPolicy.Alphabetical));
         }
     }
 }
